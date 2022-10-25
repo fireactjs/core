fe55676a624b0a9a80dc0975076a36242ca4b4dc
Chaoming Li <chaoming@users.noreply.github.com>
2022-10-25 23:09:46 +1100

    formatted code

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import firebaseJson from "./firebase.json";
 import { AuthProvider, AuthRoutes } from './lib';
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 
 function App() {
 	return (
 		<AuthProvider firebaseConfig={firebaseJson.config}>
-			<Router>
+			<BrowserRouter>
 				<Routes>
 					<Route path="/sign-in" />
 					<Route element={<AuthRoutes signInPath="/sign-in" />} >
 						<Route path="/" element={<div>signed in</div>} />
 					</Route>
 				</Routes>
-			</Router>
+			</BrowserRouter>
 		</AuthProvider>
 	)
 }
