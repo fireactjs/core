455f8f62785c4da9dc219447ad029416e520f273
Chaoming Li <email@chaoming.li>
2022-10-26 22:35:16 +1100

    added microsoft sign in button

--- src/lib/components/auth/SignIn.js
+++ src/lib/components/auth/SignIn.js
@@ -1,7 +1,8 @@
 import { Button, Container, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import googleIcon from "../../assets/images/google.svg";
+import googleSvg from "../../assets/images/google.svg";
+import microsoftSvg from "../../assets/images/microsoft.svg";
 import EmailIcon from '@mui/icons-material/Email';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
@@ -26,7 +27,7 @@ export const SignIn = ({logo, providers}) => {
                     <Typography p={2}>OR</Typography>
                 }
                 {providers && providers.google && 
-                    <Button type="button" fullWidth variant="outlined" startIcon={<img src={googleIcon} width="16" alt="Google" />} style={{marginBottom: `${btnMarginBottom}`}} size="large">
+                    <Button type="button" fullWidth variant="outlined" startIcon={<img src={googleSvg} width="16" alt="Google" />} style={{marginBottom: `${btnMarginBottom}`}} size="large">
                         <Typography component="span" style={{width: `${btWidth}`}}>
                             Sign In With Google
                         </Typography>
@@ -36,6 +37,13 @@ export const SignIn = ({logo, providers}) => {
                     <Button type="button" fullWidth variant="outlined" startIcon={<FacebookIcon style={{color: "#4267B2"}} />} style={{marginBottom: `${btnMarginBottom}`}} size="large">
                         <Typography component="span" style={{width: `${btWidth}`}}>
                             Sign In With Facebook
+                        </Typography>
+                    </Button>
+                }
+                {providers && providers.facebook && 
+                    <Button type="button" fullWidth variant="outlined" startIcon={<img src={microsoftSvg} width="16" alt="Microsoft" />} style={{marginBottom: `${btnMarginBottom}`}} size="large">
+                        <Typography component="span" style={{width: `${btWidth}`}}>
+                            Sign In With Microsoft
                         </Typography>
                     </Button>
                 }
