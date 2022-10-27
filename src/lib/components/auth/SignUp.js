83c7e689f2859e40b841e71a4809eae36b7832d3
Chaoming Li <email@chaoming.li>
2022-10-28 08:50:59 +1100

    added states in SignUp

--- src/lib/components/auth/SignUp.js
+++ src/lib/components/auth/SignUp.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 export const SignUp = ({logo, handleSuccess}) => {
+
+    const [error, setError] = useState(null);
+    const [email, setEmail] = useState("");
+    const [fullname, setFullname] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <>
             <Box>
@@ -11,9 +17,9 @@ export const SignUp = ({logo, handleSuccess}) => {
             </Box>
             <Box>
                 <Stack spacing={2} mt={2}>
-                    <TextField required fullWidth name="fullname" label="Full Name" autoComplete="name" type="text" margin="normal" />
-                    <TextField required fullWidth name="password" label="New Password" autoComplete="new-password" type="password" margin="normal" />
-                    <TextField required fullWidth name="confirm-password" label="Confirm Password" autoComplete="new-password" type="password" margin="normal" />
+                    <TextField required fullWidth name="email" label="Email" type="email" autoComplete="email" margin="normal" onChange={e => setEmail(e.target.value)} />
+                    <TextField required fullWidth name="fullname" label="Full Name" autoComplete="name" type="text" margin="normal" onChange={e => setFullname(e.target.value)} />
+                    <TextField required fullWidth name="password" label="New Password" autoComplete="new-password" type="password" margin="normal" onChange={e => setPassword(e.target.value)} />
                     <Button type="button" fullWidth variant="contained" size="large" onClick={() => handleSuccess()}>Sign Up</Button>
                 </Stack>
             </Box>
