c84bf0e4eedd14bf4c9398822219b88de19417db
Chaoming Li <email@chaoming.li>
2022-10-28 16:32:03 +1100

    disabled Sign Up button when processing

--- src/lib/components/auth/SignUp.js
+++ src/lib/components/auth/SignUp.js
@@ -9,6 +9,7 @@ export const SignUp = ({logo, successUrl, signInUrl, resetPasswordUrl}) => {
     const re = successUrl || "/"; // redirect successUrl or homepage after sign in
 
     const [error, setError] = useState(null);
+    const [processing, setProcessing] = useState(false);
     const [email, setEmail] = useState("");
     const [fullname, setFullname] = useState("");
     const [password, setPassword] = useState("");
@@ -27,7 +28,8 @@ export const SignUp = ({logo, successUrl, signInUrl, resetPasswordUrl}) => {
                     <TextField required fullWidth name="email" label="Email" type="email" autoComplete="email" margin="normal" onChange={e => setEmail(e.target.value)} />
                     <TextField required fullWidth name="fullname" label="Full Name" autoComplete="name" type="text" margin="normal" onChange={e => setFullname(e.target.value)} />
                     <TextField required fullWidth name="password" label="New Password" autoComplete="new-password" type="password" margin="normal" onChange={e => setPassword(e.target.value)} />
-                    <Button type="button" fullWidth variant="contained" size="large" onClick={() => {
+                    <Button type="button" fullWidth variant="contained" size="large" disabled={processing} onClick={() => {
+                        setProcessing(true);
                         setError(null);
                         const passwordNumericRegex = /\d+/;
                         const passwordUppercaseRegex = /[A-Z]+/;
@@ -35,11 +37,13 @@ export const SignUp = ({logo, successUrl, signInUrl, resetPasswordUrl}) => {
                         const passwordSpecialRegex = /[ !"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+/;
                         if(fullname.trim() === ""){
                             setError('Your full name is required.');
+                            setProcessing(false);
                         }else if(!passwordNumericRegex.test(String(password)) ||
                         !passwordUppercaseRegex.test(String(password)) ||
                         !passwordLowercaseRegex.test(String(password)) ||
                         !passwordSpecialRegex.test(String(password)) || password.length < 8){
                             setError('The password must contain at least 8 characters with letters (both uppercase and lowercase), numbers, and symbols.');
+                            setProcessing(false);
                         }else{;
                             const auth = getAuth();
                             createUserWithEmailAndPassword(auth, email, password).then(result => {
@@ -62,6 +66,7 @@ export const SignUp = ({logo, successUrl, signInUrl, resetPasswordUrl}) => {
                                         setError(error.message);
                                         break;
                                 }
+                                setProcessing(false);
                             });
                         }
                     }}>Sign Up</Button>
