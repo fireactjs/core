29955f26291752d384b6cbaeffd181bcb617ceb9
Chaoming Li <email@chaoming.li>
2022-10-29 08:16:50 +1100

    changed to ListItemButton

--- src/lib/components/menus/MainMenu.js
+++ src/lib/components/menus/MainMenu.js
@@ -1,4 +1,4 @@
-import { Divider, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Divider, List, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
@@ -8,18 +8,18 @@ export const MainMenu = ({profileUrl}) => {
     return (
         <List component="nav">
             <NavLink to="/" style={{textDecoration:'none'}}>
-                <ListItem button key="home">
+                <ListItemButton button key="home">
                     <ListItemIcon><HomeIcon /></ListItemIcon>
                     <ListItemText primary={<Typography color="textPrimary">My Accounts</Typography>} />
-                </ListItem>
+                </ListItemButton>
             </NavLink>
             {profileUrl && [
                 <Divider />,
                 <NavLink to={profileUrl} style={{textDecoration:'none'}}>
-                    <ListItem button key="profile">
+                    <ListItemButton button key="profile">
                         <ListItemIcon><AccountBoxIcon /></ListItemIcon>
                         <ListItemText primary={<Typography color="textPrimary">My Profile</Typography>} />
-                    </ListItem>
+                    </ListItemButton>
                 </NavLink>
             ]}
         </List>
