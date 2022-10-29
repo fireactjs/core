2a403584a88cf59d53d24537df82da686527a896
Chaoming Li <chaoming@users.noreply.github.com>
2022-10-29 11:02:49 +1100

    display user profile

--- src/lib/components/user/Profile.js
+++ src/lib/components/user/Profile.js
@@ -2,6 +2,8 @@ import { Grid, List, ListItem, Box, Avatar, Typography, Divider } from "@mui/mat
 import React from "react";
 import { AuthContext } from "../Auth";
 import EditIcon from '@mui/icons-material/Edit';
+import SendIcon from '@mui/icons-material/Send';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 
 
 export const UserProfile = () => {
@@ -11,16 +13,56 @@ export const UserProfile = () => {
                 <List component={"nav"}>
                     <ListItem>
                         <Grid container spacing={1}>
-                            <Grid container item xs={12} md={4}>
-                                <Box p={2}><strong>AVATAR</strong><br /><Typography color="textSecondary">Update via social login</Typography></Box>
+                            <Grid item xs>
+                                <Box p={1}><strong>AVATAR</strong><br /><Typography color="textSecondary">Update via social login</Typography></Box>
+                                <Box p={1}></Box>
                             </Grid>
-                            <Grid container item xs={12} md={4}>
-                                <Box p={2}></Box>
+                            <Grid item sx={{flexDirection: "column", display: "flex", justifyContent: "center"}}>
+                                <Avatar alt={context.authUser.user.displayName} src={context.authUser.user.photoURL?(context.authUser.user.photoURL):("https://ui-avatars.com/api/?name="+encodeURI(context.authUser.user.displayName)+"&background=007bff&size=64&color=f8f9fc")} style={{height:'64px',width:'64px'}} />
                             </Grid>
-                            <Grid container item xs={12} md={4}>
-                                <Box p={2} style={{marginLeft: "auto", marginRight: "0px",}}>
-                                    <Avatar alt={context.authUser.user.displayName} src={context.authUser.user.photoURL?(context.authUser.user.photoURL):("https://ui-avatars.com/api/?name="+encodeURI(context.authUser.user.displayName)+"&background=007bff&size=64&color=f8f9fc")} style={{height:'64px',width:'64px'}} />
-                                </Box>
+                        </Grid>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                        <Grid container spacing={1}>
+                            <Grid item xs>
+                                <Box p={1}><strong>NAME</strong><br />{context.authUser.user.displayName}</Box>
+                            </Grid>
+                            <Grid item sx={{flexDirection: "column", display: "flex", justifyContent: "center"}}>
+                                <EditIcon />
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                        <Grid container spacing={1}>
+                            <Grid item xs>
+                                <Box p={1}><strong>EMAIL</strong><br />{context.authUser.user.email}</Box>
+                            </Grid>
+                            <Grid item sx={{flexDirection: "column", display: "flex", justifyContent: "center"}}>
+                                <EditIcon />
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                        <Grid container spacing={1}>
+                            <Grid item xs>
+                                <Box p={1}><strong>EMAIL VERIFIED</strong><br />{(context.authUser.user.emailVerified?" Verified":"Unverified email")}</Box>
+                            </Grid>
+                            <Grid item sx={{flexDirection: "column", display: "flex", justifyContent: "center"}}>
+                            {context.authUser.user.emailVerified?(<VerifiedUserIcon />):(<SendIcon />)}
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                        <Grid container spacing={1}>
+                            <Grid item xs>
+                                <Box p={1}><strong>PASSWORD</strong><br />••••••••</Box>
+                            </Grid>
+                            <Grid item sx={{flexDirection: "column", display: "flex", justifyContent: "center"}}>
+                                <EditIcon />
                             </Grid>
                         </Grid>
                     </ListItem>
@@ -28,15 +70,7 @@ export const UserProfile = () => {
                     <ListItem button>
                         <Grid container spacing={1}>
                             <Grid container item xs={12} md={4}>
-                                <Box p={2}><strong>NAME</strong></Box>
-                            </Grid>
-                            <Grid container item xs={12} md={4}>
-                                <Box p={2}>{context.authUser.user.displayName}</Box>
-                            </Grid>
-                            <Grid container item xs={12} md={4}>
-                                <Box p={2} style={{marginLeft: "auto", marginRight: "0px",}}>
-                                    <EditIcon />
-                                </Box>
+                                <Box p={1}><Typography color="error"><strong>DELETE ACCOUNT</strong></Typography></Box>
                             </Grid>
                         </Grid>
                     </ListItem>
