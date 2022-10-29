c172b2787f744468669a5e89bc2bc88e9459cb9a
Chaoming Li <chaoming@users.noreply.github.com>
2022-10-29 14:41:23 +1100

    added update email form

--- src/lib/components/user/UserUpdateEmail.js
+++ src/lib/components/user/UserUpdateEmail.js
@@ -1,8 +1,27 @@
-import React from "react";
+import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 export const UserUpdateEmail = () => {
+    const [email, setEmail] = useState("");
+
     return (
-        <>
-        </>
+        <Container>
+            <Box p={2}>
+                <Typography component="h1" variant="h4" align="center">Update Email</Typography>
+            </Box>
+            <Box p={2}>
+                <TextField required fullWidth name="email" label="Email" type="email" autoComplete="email" margin="normal" onChange={e => setEmail(e.target.value)} />
+            </Box>
+            <Box p={2}>
+                <Grid container>
+                    <Grid item xs>
+                        <Button type="button" color="secondary" variant="outlined" >Back</Button>
+                    </Grid>
+                    <Grid item>
+                        <Button type="button" variant="contained" >Save</Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     )
 }
